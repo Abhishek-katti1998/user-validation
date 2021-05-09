5d0235c228f0b91dc481dc05a14fa8fdb617652e
Abhi7204303909 <abhishekmkatti@gmail.com>
2021-05-09 14:13:54 +0530

    'portals-addition'

--- src/Components/Modal/Modal.js
+++ src/Components/Modal/Modal.js
@@ -1,35 +1,40 @@
 import "./Modal.css";
-import BackDrop from "../Backdrop/Backdrop";
-const Modal = (props) => {
+import React from "react";
+import ReactDom from "react-dom";
+const ModalOverlay = (props) => {
   let message;
-  console.log(props.Type);
   if (props.Type === "one") {
     message = "Input cant be Non empty";
   }
   if (props.Type === "two") {
     message = "Input age should be a positive number";
   }
-  //   if (props.Type === "") {
-  //     return;
-  //   }
   const closeHandler = () => {
     document.querySelector(".Backdrop").style.opacity = 1;
     props.Fun();
   };
   return (
-    <div>
-      <div className="Modal-container">
-        <header className="Modal-header">
-          <h3 className="header">Invalid Input</h3>
-        </header>
-        <div className="Modal-content">
-          <p>{message}</p>
-        </div>
-        <button className="Modal-close" onClick={closeHandler}>
-          OK
-        </button>
+    <div className="Modal-container">
+      <header className="Modal-header">
+        <h3 className="header">Invalid Input</h3>
+      </header>
+      <div className="Modal-content">
+        <p>{message}</p>
       </div>
+      <button className="Modal-close" onClick={closeHandler}>
+        OK
+      </button>
     </div>
+  );
+};
+const Modal = (props) => {
+  return (
+    <React.Fragment>
+      {ReactDom.createPortal(
+        <ModalOverlay Fun={props.Fun} Type={props.Type} />,
+        document.querySelector("#overlay")
+      )}
+    </React.Fragment>
   );
 };
 export default Modal;
